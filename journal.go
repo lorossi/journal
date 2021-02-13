09666a5b8946893f188c56743141ca41e82d8947
lorossi <mail@lorenzoros.si>
2021-02-14 00:26:13 +0100

    fixes and updates!

--- journal.go
+++ journal.go
@@ -14,17 +14,17 @@ type Fields struct {
 }
 
 type Entry struct {
-	Title     string   `json:"title"`
-	Content   string   `json:"content"`
-	Timestamp string   `json:"timestamp"`
-	Tags      []string `json:"tags"`
-	Fields    []Fields `json:"fields"`
-	Time_obj  time.Time
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	Timestamp string    `json:"timestamp"`
+	Tags      []string  `json:"tags"`
+	Fields    []Fields  `json:"fields"`
+	Time_obj  time.Time `json:"-"`
 }
 
 type Journal struct {
-	Entries     []Entry `json:"days"`
-	Args        []string
+	Entries     []Entry  `json:"days"`
+	Args        []string `json:"-"`
 	path        string
 	time_format string
 }
@@ -32,8 +32,8 @@ type Journal struct {
 func crate_journal() Journal {
 	j := Journal{
 		path:        "database.json",
-		time_format: "2006-01-02, 03:04",
-		Args:        []string{"--add", "--remove", "--view", "--search"},
+		time_format: "2006-01-02",
+		Args:        []string{"--add", "--remove", "--view", "--search", "--edit"},
 	}
 
 	return j
@@ -71,7 +71,11 @@ func (j *Journal) load() {
 		ioutil.WriteFile(j.path, []byte("[]"), 0666)
 		return
 	}
-	_ = json.Unmarshal([]byte(file), &j)
+
+	e = json.Unmarshal([]byte(file), &j)
+	if e != nil {
+		return
+	}
 
 	for i := 0; i < len(j.Entries); i++ {
 		j.Entries[i].Time_obj, _ = time.Parse(j.time_format, j.Entries[i].Timestamp)
@@ -81,7 +85,7 @@ func (j *Journal) load() {
 // save journal to database
 func (j *Journal) save() {
 	// Unmarshal data
-	JSON_bytes, _ := json.MarshalIndent(j.Entries, "", "  ")
+	JSON_bytes, _ := json.MarshalIndent(j, "", "  ")
 	// write to file
 	_ = ioutil.WriteFile(j.path, JSON_bytes, 0666)
 }
@@ -93,46 +97,61 @@ func (j *Journal) createEntry(entry string) {
 	var current_delimiter string
 	// title, content variables
 	var title, content string
-	// tags variable
-	var tags []string
+	// entry split by words, tags variables
+	var words, tags []string
 	// current datetime variable
 	var time_obj time.Time
 	// new entry variable
 	var new_entry Entry
+
+	// check if the first word is either today or yesterday
+	switch words = strings.Split(entry, " "); strings.ToLower(words[0]) {
+	case "yesterday:":
+		time_obj = time.Now().AddDate(0, 0, -1)
+		content = strings.Join(words[1:], " ")
+	case "today:":
+		content = strings.Join(words[1:], " ")
+		time_obj = time.Now()
+	default:
+		// try to parse time
+		var e error
+		time_obj, e = time.Parse(j.time_format, strings.TrimSpace(words[0]))
+		if e == nil {
+			content = strings.Join(words[1:], " ")
+		} else {
+			// failed, time is now
+			content = entry
+			time_obj = time.Now()
+		}
+	}
 
 	// find the delimiter between title and content
 	current_delimiter = find_delimiter(entry, delimiters)
 
 	if current_delimiter == "" {
 		// the title is the whole entry
-		title = strings.TrimSpace(entry)
+		title = strings.TrimSpace(content)
 	} else {
-		split_entry := strings.Split(entry, current_delimiter)
+		split_entry := strings.Split(content, current_delimiter)
 		// the title is the first part BEFORE the delimiter
 		title = strings.TrimSpace(split_entry[0] + current_delimiter)
 	}
 
 	// now load the tags
-	if strings.Contains(entry, "+") {
+	if strings.Contains(content, "+") {
 		// we found one or more tags
-		tags = strings.Split(entry, "+")[1:]
+		tags = strings.Split(content, "+")[1:]
 	}
 
-	// the content is the whole entry
-	// maybe we should filter out all the tags?
-	content = entry
-
-	// load current time
-	time_obj = time.Now()
 	// finally, generate and add the new entry
 	new_entry = j.createNewEntry(title, content, tags, time_obj)
 	j.addNewEntry(new_entry)
 }
 
-func (j *Journal) showDay(Timestamp string) {
+func (j *Journal) showDay(timestamp string) {
 	for _, e := range j.Entries {
-		if e.Timestamp == Timestamp {
-			fmt.Println("Date: ", e.Time_obj)
+		if e.Timestamp == timestamp {
+			fmt.Println("Date: ", e.Timestamp)
 			fmt.Println("Title: ", e.Title)
 			fmt.Println("Content: ", e.Content)
 			fmt.Println("Tags:")
@@ -143,7 +162,7 @@ func (j *Journal) showDay(Timestamp string) {
 			for _, f := range e.Fields {
 				fmt.Println("\t", f.Key, ": ", f.Value)
 			}
-			break
+			return
 		}
 	}
 }
--- main.go
+++ main.go
@@ -11,8 +11,6 @@ func main() {
 	j := crate_journal()
 	// load from database
 	j.load()
-	// declare new entry that will be parsed from console
-	var entry string
 
 	// loop thought all arguments
 	switch args := len(os.Args); {
@@ -24,24 +22,25 @@ func main() {
 
 	case args > 1:
 		// detect the passed arg
-		switch param := strings.ToLower(os.Args[1]); param {
+		switch strings.ToLower(os.Args[1]) {
 		case "--remove":
 			fmt.Println("REMOVE")
 		case "--view":
-			fmt.Println("VIEW")
+			j.showDay(os.Args[2])
 		case "--search":
 			fmt.Println("SEARCH")
 		case "--add":
 			// the user has provided the "--add" flag. Time to add an entry.
-			entry = strings.Join(os.Args[2:], " ")
+			entry := strings.Join(os.Args[2:], " ")
 			j.createEntry(entry)
+			// save journal
+			j.save()
 		default:
 			// the user has provided more than one word. Time to add an entry.
-			entry = strings.Join(os.Args[1:], " ")
+			entry := strings.Join(os.Args[1:], " ")
 			j.createEntry(entry)
+			// save journal
+			j.save()
 		}
 	}
-
-	// save journal
-	j.save()
 }
