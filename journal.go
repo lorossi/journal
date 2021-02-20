6300968b3e2027e1003c08bd5830dde0a9d0ee2d
lorossi <mail@lorenzoros.si>
2021-02-21 00:57:03 +0100

    bug fixes and refactoring

--- journal.go
+++ journal.go
@@ -17,24 +17,26 @@ import (
 	"time"
 )
 
+// Entry contains a single entry in the journal
 type Entry struct {
 	Title     string            `json:"title"`
 	Content   string            `json:"content"`
 	Timestamp string            `json:"timestamp"`
 	Tags      []string          `json:"tags"`
 	Fields    map[string]string `json:"fields"`
-	time_obj  time.Time
+	timeObj   time.Time
 }
 
+// Journal is the class containing the whole journal
 type Journal struct {
-	Entries     []Entry `json:"days"`
-	Last_loaded string  `json:"last_loaded"`
-	Created     string  `json:"created"`
-	Version     string  `json:"version"`
-	Repo        string  `json:"-"`
-	password    string
-	path        string
-	time_format string
+	Entries    []Entry `json:"days"`
+	LastLoaded string  `json:"LastLoaded"`
+	Created    string  `json:"created"`
+	Version    string  `json:"version"`
+	Repo       string  `json:"-"`
+	password   string
+	path       string
+	timeFormat string
 }
 
 // setters
@@ -43,7 +45,7 @@ func (j *Journal) setPassword(password string) {
 }
 
 // getters
-func (j *Journal) getCurrentVersion() (current_version string, e error) {
+func (j *Journal) getCurrentVersion() (currentVersion string, e error) {
 	// set a timeout
 	client := &http.Client{
 		Timeout: 5 * time.Second,
@@ -54,43 +56,48 @@ func (j *Journal) getCurrentVersion() (current_version string, e error) {
 		return "", e
 	}
 	// get redirect
-	final_url := strings.Split(response.Request.URL.String(), "/")
-	if len(final_url) == 0 {
+	finalURL := strings.Split(response.Request.URL.String(), "/")
+	if len(finalURL) == 0 {
 		return "", errors.New("unable to fetch new version")
 	}
 	// get the version tag
-	current_version = final_url[len(final_url)-1]
+	currentVersion = finalURL[len(finalURL)-1]
 	// remove the v
-	current_version = current_version[1:]
+	currentVersion = currentVersion[1:]
 
-	return current_version, e
+	return currentVersion, e
 }
 
+<<<<<<< Updated upstream:journal.go
 func create_journal() (j Journal, e error) {
+=======
+// NewJournal returns a new empty journal. Also initializes the folder
+func NewJournal() (j Journal, e error) {
+>>>>>>> Stashed changes:journal/journal.go
 	// create journal path if it does not exist
-	var journal_folder string
+	var journalFolder string
 	// multi os support (hopefully)
 	if runtime.GOOS == "linux" {
-		journal_folder = "/var/lib/journal"
+		journalFolder = "/var/lib/journal"
 	} else if runtime.GOOS == "darwin" {
 		// macOS, might need testing
-		journal_folder = "~/Library/Preferences/journal"
+		journalFolder = "~/Library/Preferences/journal"
 	} else if runtime.GOOS == "windows" {
-		journal_folder = os.Getenv("APPDATA") + "\\journal"
+		journalFolder = os.Getenv("APPDATA") + "\\journal"
 	}
 
-	if _, e := os.Stat(journal_folder); os.IsNotExist(e) {
-		e := os.Mkdir(journal_folder, 0777)
+	if _, e := os.Stat(journalFolder); os.IsNotExist(e) {
+		e := os.Mkdir(journalFolder, 0777)
 		fmt.Println(e)
-		return Journal{}, errors.New("cannot create folder " + journal_folder)
+		return Journal{}, errors.New("cannot create folder " + journalFolder)
 	}
 
 	j = Journal{
-		Last_loaded: time.Now().Format(time.RFC3339),
-		Version:     "1.1.0",
-		Repo:        "https://github.com/lorossi/go-journal",
-		time_format: "2006-01-02 15:04:05",
-		path:        journal_folder + "/journal.json",
+		LastLoaded: time.Now().Format(time.RFC3339),
+		Version:    "1.1.0",
+		Repo:       "https://github.com/lorossi/go-journal",
+		timeFormat: "2006-01-02 15:04:05",
+		path:       journalFolder + "/journal.json",
 	}
 
 	if strings.Contains(j.Version, "b") {
@@ -100,7 +107,7 @@ func create_journal() (j Journal, e error) {
 	return j, nil
 }
 
-func read_from_file(path string) (file []byte, e error) {
+func readFromFile(path string) (file []byte, e error) {
 	file, e = ioutil.ReadFile(path)
 
 	if e != nil {
@@ -109,7 +116,7 @@ func read_from_file(path string) (file []byte, e error) {
 	return file, e
 }
 
-func write_to_file(path string, bytes []byte) (e error) {
+func writeToFile(path string, bytes []byte) (e error) {
 	e = ioutil.WriteFile(path, bytes, 0666)
 	if e != nil {
 		return errors.New("error while working with the file. cannot save")
@@ -118,10 +125,10 @@ func write_to_file(path string, bytes []byte) (e error) {
 }
 
 // package the variables into a new entry
-func (j *Journal) createNewEntry(title, content string, tags []string, fields map[string]string, time_obj time.Time) (entry Entry) {
+func (j *Journal) createNewEntry(title, content string, tags []string, fields map[string]string, timeObj time.Time) (entry Entry) {
 	var timestamp string
 	// format the timestamp
-	timestamp = time_obj.Format(j.time_format)
+	timestamp = timeObj.Format(j.timeFormat)
 	// create the new entry
 	entry = Entry{
 		Title:     title,
@@ -129,7 +136,7 @@ func (j *Journal) createNewEntry(title, content string, tags []string, fields ma
 		Tags:      tags,
 		Fields:    fields,
 		Timestamp: timestamp,
-		time_obj:  time_obj,
+		timeObj:   timeObj,
 	}
 
 	return entry
@@ -139,7 +146,7 @@ func (j *Journal) createNewEntry(title, content string, tags []string, fields ma
 func (j *Journal) load() (e error) {
 	var file []byte
 	// try to open the file
-	file, e = read_from_file(j.path)
+	file, e = readFromFile(j.path)
 	// if not available, create an empty one
 	if e != nil {
 		j.Created = time.Now().Format(time.RFC3339)
@@ -154,7 +161,7 @@ func (j *Journal) load() (e error) {
 	}
 	// calculate the time for each entry
 	for i := 0; i < len(j.Entries); i++ {
-		j.Entries[i].time_obj, _ = time.Parse(j.time_format, j.Entries[i].Timestamp)
+		j.Entries[i].timeObj, _ = time.Parse(j.timeFormat, j.Entries[i].Timestamp)
 	}
 
 	return nil
@@ -164,7 +171,7 @@ func (j *Journal) load() (e error) {
 func (j *Journal) decrypt() (e error) {
 	var file []byte
 	// try to open the file
-	file, e = read_from_file(j.path)
+	file, e = readFromFile(j.path)
 	if e != nil {
 		return errors.New("cannot open encrypted database")
 	}
@@ -181,12 +188,12 @@ func (j *Journal) decrypt() (e error) {
 		return errors.New("cannot create new GCM")
 	}
 
-	nonce_size := gcm.NonceSize()
-	if len(file) < nonce_size {
+	nonceSize := gcm.NonceSize()
+	if len(file) < nonceSize {
 		return errors.New("file is too short")
 	}
 
-	nonce, ciphertext := file[:nonce_size], file[nonce_size:]
+	nonce, ciphertext := file[:nonceSize], file[nonceSize:]
 	plaintext, e := gcm.Open(nil, nonce, ciphertext, nil)
 	if e != nil {
 		return errors.New("cannot decode file")
@@ -199,7 +206,7 @@ func (j *Journal) decrypt() (e error) {
 	}
 	// calculate the time for each entry
 	for i := 0; i < len(j.Entries); i++ {
-		j.Entries[i].time_obj, _ = time.Parse(j.time_format, j.Entries[i].Timestamp)
+		j.Entries[i].timeObj, _ = time.Parse(j.timeFormat, j.Entries[i].Timestamp)
 	}
 
 	return nil
@@ -208,19 +215,19 @@ func (j *Journal) decrypt() (e error) {
 // save journal to database
 func (j *Journal) save() (e error) {
 	// Unmarshal data
-	JSON_bytes, e := json.MarshalIndent(j, "", "  ")
+	JSONbytes, e := json.MarshalIndent(j, "", "  ")
 	if e != nil {
 		return errors.New("error while encoding data. cannot save")
 	}
 	// write to file
-	write_to_file(j.path, JSON_bytes)
+	writeToFile(j.path, JSONbytes)
 	return e
 }
 
 func (j *Journal) encrypt() (e error) {
 	key := []byte(j.password)
 
-	JSON_bytes, e := json.MarshalIndent(j, "", "  ")
+	JSONbytes, e := json.MarshalIndent(j, "", "  ")
 	if e != nil {
 		return errors.New("error while encoding data. cannot save")
 	}
@@ -240,9 +247,9 @@ func (j *Journal) encrypt() (e error) {
 		return errors.New("cannot create new random sequence")
 	}
 
-	ciphertext := gcm.Seal(nonce, nonce, JSON_bytes, nil)
+	ciphertext := gcm.Seal(nonce, nonce, JSONbytes, nil)
 	// write to file
-	e = write_to_file(j.path, ciphertext)
+	e = writeToFile(j.path, ciphertext)
 	return e
 }
 
@@ -250,7 +257,7 @@ func (j *Journal) encrypt() (e error) {
 func (j *Journal) createEntry(entry string) {
 	// array of separators that end the title
 	var delimiters = []string{".", "?", "!", "+", "@"}
-	var current_delimiter string
+	var currentDelimiter string
 	// title, content variables
 	var title, content string
 	// entry tags variable
@@ -259,36 +266,36 @@ func (j *Journal) createEntry(entry string) {
 	var fields map[string]string
 	fields = make(map[string]string)
 	// current datetime variable
-	var new_date time.Time
+	var newDate time.Time
 	// new entry variable
-	var new_entry Entry
+	var newEntry Entry
 
 	// find the submitted date and the entry without the (eventual) date
-	content, new_date = parse_entry(entry, j.time_format)
+	content, newDate = parseEntry(entry, j.timeFormat)
 	if content == "" {
 		return
 	}
 
 	// find the delimiter between title and content
-	current_delimiter = find_delimiter(content, delimiters)
+	currentDelimiter = findDelimiter(content, delimiters)
 
-	if current_delimiter == "" {
+	if currentDelimiter == "" {
 		// the title is the whole entry
 		title = strings.TrimSpace(content)
 		content = ""
 	} else {
-		split_entry := strings.Split(content, current_delimiter)
+		splitEntry := strings.Split(content, currentDelimiter)
 		// the title is the first part BEFORE the delimiter
-		title = strings.TrimSpace(split_entry[0] + current_delimiter)
+		title = strings.TrimSpace(splitEntry[0] + currentDelimiter)
 		content = strings.Replace(content, title, "", 1)
 	}
 
 	// now load the tags
 	if strings.Contains(content, "+") {
 		// we found one or more tags
-		string_tags := strings.Split(content, "+")[1:]
+		stringTags := strings.Split(content, "+")[1:]
 
-		for _, s := range string_tags {
+		for _, s := range stringTags {
 			tags = append(tags, strings.Split(s, " ")[0])
 		}
 
@@ -302,55 +309,58 @@ func (j *Journal) createEntry(entry string) {
 	// now load the fields
 	if strings.Contains(content, "@") {
 		// we found one or more fields
-		string_fields := strings.Split(content, "@")[1:]
+		sstringFields := strings.Split(content, "@")[1:]
 
-		for i := 0; i < len(string_fields); i++ {
-			values := strings.Split(string_fields[i], "=")
+		for i := 0; i < len(sstringFields); i++ {
+			values := strings.Split(sstringFields[i], "=")
 			// if there arent' exactly 2 strings (key, value) separated by semicolon,
 			// something is wrong
 			if len(values) != 2 {
-				print_error(errors.New("field '"+values[0]+"' provided in a wrong format"), 1)
+				printError(errors.New("field '"+values[0]+"' provided in a wrong format"), 1)
 			} else {
 				fields[values[0]] = strings.TrimSpace(values[1])
 			}
 
 			// now remove all fields from content
-			for i := 0; i < len(string_fields); i++ {
-				content = strings.ReplaceAll(content, "@"+string_fields[i], "")
+			for i := 0; i < len(sstringFields); i++ {
+				content = strings.ReplaceAll(content, "@"+sstringFields[i], "")
 			}
 		}
 	}
 	// remove leading / trailing spaces for a better Format
 	content = strings.TrimSpace(content)
 	// remove all multiple spaces
-	content = remove_multiple_spaces(content)
+	content = removeMultipleSpaces(content)
 
 	// finally, generate the new entry
-	new_entry = j.createNewEntry(title, content, tags, fields, new_date)
+	newEntry = j.createNewEntry(title, content, tags, fields, newDate)
 	// append the entry to the entries array
-	j.Entries = append(j.Entries, new_entry)
+	j.Entries = append(j.Entries, newEntry)
 	// sort the entries array
-	sort.Slice(j.Entries, func(i, k int) bool { return j.Entries[i].time_obj.Before(j.Entries[k].time_obj) })
+	sort.Slice(j.Entries, func(i, k int) bool { return j.Entries[i].timeObj.Before(j.Entries[k].timeObj) })
 }
 
 func (j *Journal) removeEntry(timestamp string) (e error) {
-	var remove_date time.Time
+	var removeDate time.Time
 	var level int
-	var clean_entries []Entry
+	var cleanEntries []Entry
 
 	// get the date from the string
-	remove_date, level = parse_day(timestamp)
+	removeDate, level = parseDay(timestamp)
 	if level == -1 {
 		return errors.New("date was not provided correctly")
 	}
 
+	fmt.Println(removeDate, level)
+
 	// init an empty slice of entries
-	clean_entries = make([]Entry, 0)
+	cleanEntries = make([]Entry, 0)
 	for _, e := range j.Entries {
 		// if the entry has the same date as the timestamp, don't append it
 		// to the new slice of entries
 
 		switch level {
+<<<<<<< Updated upstream:journal.go
 		case 0:
 			if !same_day(e.time_obj, remove_date) {
 				clean_entries = append(clean_entries, e)
@@ -362,41 +372,58 @@ func (j *Journal) removeEntry(timestamp string) (e error) {
 		case 2:
 			if !same_year(e.time_obj, remove_date) {
 				clean_entries = append(clean_entries, e)
+=======
+		case 1:
+			if !sameDay(e.timeObj, removeDate) {
+				cleanEntries = append(cleanEntries, e)
+			}
+		case 2:
+			if !sameMonth(e.timeObj, removeDate) {
+				cleanEntries = append(cleanEntries, e)
+			}
+		case 3:
+			if !sameYear(e.timeObj, removeDate) {
+				cleanEntries = append(cleanEntries, e)
+>>>>>>> Stashed changes:journal/journal.go
 			}
 		}
 	}
 
-	if len(clean_entries) == len(j.Entries) {
+	if len(cleanEntries) == len(j.Entries) {
 		// no entries were removed
 		return errors.New("entry not found")
-	} else {
-		// replace the entries with a new slice
-		j.Entries = clean_entries
-		return nil
 	}
+	// replace the entries with a new slice
+	j.Entries = cleanEntries
+	return nil
+
 }
 
 func (j *Journal) showEntries(timestamp string) (entries []Entry, e error) {
-	var get_date time.Time
+	var getDate time.Time
 	var level int
 
 	entries = make([]Entry, 0)
+<<<<<<< Updated upstream:journal.go
 	get_date, level = parse_day(timestamp)
 	fmt.Println(get_date, level)
+=======
+	getDate, level = parseDay(timestamp)
+>>>>>>> Stashed changes:journal/journal.go
 
 	// loop throught every entry and look for one with the desired day
 	for _, e := range j.Entries {
 		switch level {
 		case 1:
-			if same_day(e.time_obj, get_date) {
+			if sameDay(e.timeObj, getDate) {
 				entries = append(entries, e)
 			}
 		case 2:
-			if same_month(e.time_obj, get_date) {
+			if sameMonth(e.timeObj, getDate) {
 				entries = append(entries, e)
 			}
 		case 3:
-			if same_year(e.time_obj, get_date) {
+			if sameYear(e.timeObj, getDate) {
 				entries = append(entries, e)
 			}
 		}
@@ -415,67 +442,68 @@ func (j *Journal) showEntries(timestamp string) (entries []Entry, e error) {
 func (j *Journal) getAllEntries() ([]Entry, error) {
 	if len(j.Entries) > 0 {
 		return j.Entries, nil
-	} else {
-		// if there are no entries, return the empty slice and set an error
-		return make([]Entry, 0), errors.New("no entries found")
 	}
+
+	// if there are no entries, return the empty slice and set an error
+	return make([]Entry, 0), errors.New("no entries found")
+
 }
 
-func (j *Journal) removeEntriesBetween(start_timestamp, end_timestamp string) (e error) {
+func (j *Journal) removeEntriesBetween(startTimestamp, endTimestamp string) (e error) {
 	var start, end time.Time
-	var clean_entries []Entry
+	var cleanEntries []Entry
 
-	start, e = time.Parse("2006-01-02", start_timestamp)
+	start, e = time.Parse("2006-01-02", startTimestamp)
 	if e != nil {
 		return errors.New("cannot parse start date")
 	}
-	end, e = time.Parse("2006-01-02", end_timestamp)
+	end, e = time.Parse("2006-01-02", endTimestamp)
 	if e != nil {
 		return errors.New("cannot parse end date")
 	}
 
 	for _, entry := range j.Entries {
-		if !date_between(entry.time_obj, start, end) {
-			clean_entries = append(clean_entries, entry)
+		if !dateBetween(entry.timeObj, start, end) {
+			cleanEntries = append(cleanEntries, entry)
 		}
 	}
 
-	if len(clean_entries) == len(j.Entries) {
+	if len(cleanEntries) == len(j.Entries) {
 		// no entries were removed
 		return errors.New("entries not found")
-	} else {
-		// replace the entries with a new slice
-		j.Entries = clean_entries
-		return nil
 	}
+	// replace the entries with a new slice
+	j.Entries = cleanEntries
+	return nil
+
 }
 
 func (j *Journal) removeAllEntries() {
 	j.Entries = make([]Entry, 0)
 }
 
-func (j *Journal) getEntriesBetween(start_timestamp, end_timestamp string) (entries []Entry, e error) {
+func (j *Journal) getEntriesBetween(startTimestamp, endTimestamp string) (entries []Entry, e error) {
 	var start, end time.Time
-	start, e = time.Parse("2006-01-02", start_timestamp)
+	start, e = time.Parse("2006-01-02", startTimestamp)
 	if e != nil {
 		return make([]Entry, 0), errors.New("cannot parse start date")
 	}
-	end, e = time.Parse("2006-01-02", end_timestamp)
+	end, e = time.Parse("2006-01-02", endTimestamp)
 	if e != nil {
 		return make([]Entry, 0), errors.New("cannot parse end date")
 	}
 
 	for _, entry := range j.Entries {
-		if date_between(entry.time_obj, start, end) {
+		if dateBetween(entry.timeObj, start, end) {
 			entries = append(entries, entry)
 		}
 	}
 
 	if len(entries) > 0 {
 		return entries, nil
-	} else {
-		return make([]Entry, 0), errors.New("no entries found between those dates")
 	}
+	return make([]Entry, 0), errors.New("no entries found between those dates")
+
 }
 
 func (j *Journal) searchKeywords(keywords []string) (entries []Entry, e error) {
@@ -489,17 +517,17 @@ func (j *Journal) searchKeywords(keywords []string) (entries []Entry, e error) {
 
 	if len(entries) > 0 {
 		return entries, nil
-	} else {
-		return make([]Entry, 0), errors.New("no entries found with the keyword")
 	}
+	return make([]Entry, 0), errors.New("no entries found with the keyword")
+
 }
 
 func (j *Journal) searchTags(tags []string) ([]Entry, error) {
 	var entries []Entry
 	for _, entry := range j.Entries {
-		for _, entry_tag := range entry.Tags {
+		for _, entryTag := range entry.Tags {
 			for _, t := range tags {
-				if entry_tag == t {
+				if entryTag == t {
 					entries = append(entries, entry)
 					break
 				}
@@ -509,9 +537,9 @@ func (j *Journal) searchTags(tags []string) ([]Entry, error) {
 
 	if len(entries) > 0 {
 		return entries, nil
-	} else {
-		return make([]Entry, 0), errors.New("no entries found with the tag")
 	}
+	return make([]Entry, 0), errors.New("no entries found with the tag")
+
 }
 
 func (j *Journal) searchFields(keys []string) (entries []Entry, e error) {
@@ -525,24 +553,24 @@ func (j *Journal) searchFields(keys []string) (entries []Entry, e error) {
 
 	if len(entries) > 0 {
 		return entries, nil
-	} else {
-		return make([]Entry, 0), errors.New("no entries found with the field")
 	}
+	return make([]Entry, 0), errors.New("no entries found with the field")
+
 }
 
 func (j *Journal) getAllTags() (tags map[string]int, e error) {
 	tags = make(map[string]int)
 	for _, entry := range j.Entries {
 		for _, tag := range entry.Tags {
-			tags[tag] += 1
+			tags[tag]++
 		}
 	}
 
 	if len(tags) > 0 {
 		return tags, nil
-	} else {
-		return make(map[string]int), errors.New("no tags found")
 	}
+	return make(map[string]int), errors.New("no tags found")
+
 }
 
 func (j *Journal) getAllFields() (fields []map[string]string, e error) {
@@ -554,7 +582,7 @@ func (j *Journal) getAllFields() (fields []map[string]string, e error) {
 
 	if len(fields) > 0 {
 		return fields, nil
-	} else {
-		return make([]map[string]string, 0), errors.New("no fields found")
 	}
+	return make([]map[string]string, 0), errors.New("no fields found")
+
 }
