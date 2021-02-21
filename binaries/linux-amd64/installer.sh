a8b6db48e7ee70e34fd828366f744cfbf7723040
lorossi <mail@lorenzoros.si>
2021-02-21 01:12:57 +0100

    updated binaries and moved them in the correct folder

---
+++ binaries/linux-amd64/installer.sh
@@ -0,0 +1,3 @@
+#move the binary to PATH folder to be able to run it everywhere
+chmod +x journal
+cp journal /usr/local/bin/journal
--- binaries/windows/installer.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-IF NOT EXIST %APPDATA%"\journal" md %APPDATA%"\journal"
-copy journal.exe %APPDATA%"\journal\journal.exe"
-SET PATH=%PATH%;%APPDATA%"\journal\journal.exe"
-echo Done
