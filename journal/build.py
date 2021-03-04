59580e71f7d6f6b8ed3bc2bbc6054fc69d893bbb
lorossi <contact@lorenzoros.si>
2021-03-04 22:36:47 +0100

    Update build.py

--- journal/build.py
+++ journal/build.py
@@ -1,14 +1,20 @@
 # this script automatically builds and zips the package for multiple
 # architectures and operating systems.
 # it is really basic, nothing too fancu.
-# PEP8 compliant BTW
+# (almost) PEP8 compliant BTW
 import os
 import json
 import zipfile
 import subprocess
 
 
-def build(binaries_folder="../binaries/", settings_file="buildsettings.json"):
+def build(binaries_folder="../binaries/", settings_file="buildsettings.json", compressed_filename="builds.zip"):
+    try:
+        os.remove(binaries_folder + compressed_filename)
+    except FileNotFoundError:
+        pass
+    except Exception as e:
+        print(e)
 
     with open(settings_file) as f:
         settings = json.load(f)
@@ -16,12 +22,12 @@ def build(binaries_folder="../binaries/", settings_file="buildsettings.json"):
     for b in settings["builds"]:
         if not os.path.exists(binaries_folder + b["folder"]):
             os.makedirs(binaries_folder + b["folder"])
-        options = f"env GOOS={b['os']} GOARCH={b['architecture']} go build -o " \
-                  f"{binaries_folder}{b['folder']}{b['filename']}"
+        options = f"env GOOS={b['os']} GOARCH={b['architecture']} go build " \
+                  f"-o {binaries_folder}{b['folder']}{b['filename']}"
 
         subprocess.run(options.split(" "))
 
-    zipf = zipfile.ZipFile(binaries_folder + "binaries.zip", "w",
+    zipf = zipfile.ZipFile(binaries_folder + compressed_filename, "w",
                            zipfile.ZIP_DEFLATED)
     for root, dirs, files in os.walk(binaries_folder):
         for file in files:
